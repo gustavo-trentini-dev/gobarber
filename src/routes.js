2c99813e8857d4a20d598bf06c8ad38f8a3bf5b5
Gustavo Trentini <gustavo.trentini@universo.univates.br>
2020-01-30 20:04:35 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1235666',
+  });
+
+  return res.json(user);
+});
+
+export default routes;
