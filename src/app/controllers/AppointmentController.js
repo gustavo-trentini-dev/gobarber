da68bc374a49198c8fe710c78bf7dfd7d5c3691b
Gustavo Trentini <gustavo.trentini@universo.univates.br>
2020-02-04 00:28:23 -0300

    BackEnd Node finalizado e variaveis de ambiente configuradas

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -17,7 +18,7 @@ class AppointmentController {
         user_id: req.userId,
         canceled_at: null,
       },
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       order: ['date'],
       limit: 20,
       offset: (page - 1) * 20,
@@ -119,7 +120,18 @@ class AppointmentController {
 
   async delete(req, res) {
     const appointment = await Appointment.findByPk(req.params.id, {
-      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
     });
 
     if (appointment.user_id !== req.userId) {
@@ -140,11 +152,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -12,8 +12,6 @@ export default async (req, res, next) => {
 
   const [, token] = authHeader.split(' ');
 
-  console.log(token);
-
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
